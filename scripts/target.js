ef898c88473cc5182e31d50e41fa61e9c7d02663
James Baicoianu <james_github@baicoianu.com>
2015-12-30 19:08:49 -0800

    Improved label alignment and collision detection

--- scripts/target.js
+++ scripts/target.js
@@ -1,15 +1,28 @@
 elation.require([], function() {
   elation.component.add('engine.things.snake_target', function() {
     this.createObject3D = function() {
-return new THREE.Object3D();
-      var geo = new THREE.SphereGeometry(0.75);
-      var mat = new THREE.MeshPhongMaterial({color: 0x00ff00, transparent: true, opacity: .4});
-      return new THREE.Mesh(geo, mat);
+      var geo = new THREE.SphereGeometry(.5);
+      var mat = new THREE.MeshPhongMaterial({color: 0x00ff00, transparent: true, opacity: 0});
+      var mesh = new THREE.Mesh(geo, mat);
+      this.light = new THREE.PointLight(0xffff00, .5, 10);
+      mesh.add(this.light);
+      this.light.position.z = 2;
+      return mesh;
+    }
+    this.createChildren = function() {
     }
     this.setLabel = function(label) {
       if (this.label) this.remove(this.label);
       //this.label = new THREE.Mesh(new THREE.TextGeometry(label), new THREE.MeshPhongMaterial({color: 0xccccc}));
-      this.label = this.spawn('label', 'label_' + label, { text: label, size: 1.25, position: [-.1, 0, 0] });
+      this.label = this.spawn('label', 'label_' + label, {
+        text: label, 
+        size: 1.25,
+        thickness: 0.85, 
+        font: 'Graph 35+ pix',
+        align: 'center',
+        verticalalign: 'middle',
+        zalign: 'back'
+      });
     }
   }, elation.engine.things.generic);
 });
